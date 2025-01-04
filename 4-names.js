08d3da0cb44444b24c199f4bc314e7c8aa960737
MukeshKumar2103 <mukesh2132000@gmail.com>
2025-01-04 18:30:13 +0530

    modules lession completed

---
+++ 4-names.js
@@ -0,0 +1,11 @@
+// local (this is only available in this file)
+const secret = 'SUPER SECRET'
+
+// share (this is available in other files as well reson is we are exporting this)
+const kumar = 'kumar';
+const peter = 'Peter';
+
+
+module.exports = {kumar, peter};
+
+// console.log({secret,module,exports:module.exports});
---
+++ 6-alternative-flavor.js
@@ -0,0 +1,5 @@
+module.exports.items = ['items1', 'items2', 'items3', 'items4', 'items5', 'items6', 'items7', 'items8', 'items9', 'items10'];
+
+const person = {name: 'John Doe'};
+
+module.exports.singlePerson = person;
